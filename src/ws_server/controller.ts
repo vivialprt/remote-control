63ae6c6ccddeaf0aec009c15ba601c420e9f2730
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2023-01-22 18:41:30 +0200

    feat: mouse moving

--- src/ws_server/controller.ts
+++ src/ws_server/controller.ts
@@ -1,21 +1,25 @@
+import { mouse, up, down, left, right } from "@nut-tree/nut-js";
 import { RawData } from "ws";
 
 
 export async function controller(msg: RawData): Promise<string> {
     const [cmd, ...args] = msg.toString().split(' ');
-    console.log(cmd);
 
     switch (cmd) {
         case 'mouse_up':
+            await mouse.move(up(Number(args[0])));
             break;
 
         case 'mouse_down':
+            await mouse.move(down(Number(args[0])));
             break;
 
         case 'mouse_left':
+            await mouse.move(left(Number(args[0])));
             break;
 
         case 'mouse_right':
+            await mouse.move(right(Number(args[0])));
             break;
 
         case 'mouse_position':
